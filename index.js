9c9ec60f9b29930efce1d8c988f5d8883bf8afae
Raunak Ramdas Bandodcar <raunakhome@Raunaks-MacBook-Pro.local>
2025-09-28 15:57:27 +0100

    work done until disable button

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const nextButton = document.querySelector(".nextQuestion");
 const optionHeader = document.getElementById("optionIn");
 const image = document.getElementById("imageIC");
 const questionCount = document.getElementById("questionCount");
+const slider = document.getElementById("inputSlider");
 
 let currentQuestionIndex = 0;
 let currenQuestion = 1;
@@ -31,22 +32,58 @@ fetch("data.json")
 function getData(data) {
   informationData = data;
   handleQuestion(currentQuestionIndex, data);
+  nextButton.classList.add("disableButton");
 }
 
 function handleQuestion(indexx, info) {
   const letterArray = ["A", "B", "C", "D"];
-  questionContainer.innerHTML = `<h1 class="question-title">${info.quizzes[0].questions[indexx]?.question}</h1>`;
+
+  if (info.quizzes[0].questions[indexx]?.question != undefined) {
+    questionContainer.innerHTML = `<h1 class="question-title">${info.quizzes[0].questions[indexx].question}</h1>`;
+  }
+
   optionHeader.textContent = info.quizzes[0].title;
   image.src = info.quizzes[0].icon;
   questionCount.innerText = `Question ${currenQuestion} of ${info.quizzes[0].questions.length}`;
 
-  optionContainer.forEach(
-    (item, index) =>
-      (item.innerHTML = `<span id="optionSpan">${letterArray[index]}</span> <p class="answerText">${info.quizzes[0].questions[indexx].options[index]}</p>`)
-  );
+  slider.value = indexx;
+  slider.max = info.quizzes[0].questions.length - 1;
+  const percentage = ((indexx - slider.min) / (slider.max - slider.min)) * 100;
+
+  slider.style.setProperty("--value", percentage + "%");
+
+  optionContainer.forEach((item, index) => {
+    if (info?.quizzes[0]?.questions[indexx]?.options[index] != undefined) {
+      item.innerHTML = `<span id="optionSpan">${letterArray[index]}</span> <p class="answerText">${info?.quizzes[0]?.questions[indexx]?.options[index]}</p>`;
+      item.style.cursor = "pointer";
+      item.classList.remove("active");
+    }
+  });
 }
 
-nextButton.addEventListener("click", () => {
+function handleActive() {
+  optionContainer.forEach((item, index) => {
+    item.addEventListener("click", (event) => {
+      optionContainer.forEach((option) => {
+        option.classList.remove("active");
+
+        option.querySelector("#optionSpan").style.backgroundColor = "#edf1f9";
+        option.querySelector("#optionSpan").style.color = "#626c7f";
+      });
+      item.classList.add("active");
+      item.querySelector("#optionSpan").style.backgroundColor = "#a729f5";
+      item.querySelector("#optionSpan").style.color = "#ffffff";
+    });
+  });
+}
+
+handleActive();
+
+//Next Button Functionality
+
+nextButton.disabled = true;
+
+nextButton.addEventListener("click", (event) => {
   currentQuestionIndex++;
 
   if (currenQuestion <= 9) {
